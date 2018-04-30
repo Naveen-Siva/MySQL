53685bf1f6d749e50bb8755fa209d28e905bd52f
Naveen-Siva <38775912+Naveen-Siva@users.noreply.github.com>
2018-04-30 05:02:15 +0000

    Update and rename .gitignore to create_table.sql

---
+++ .gitignore/create_table.sql
@@ -0,0 +1,4 @@
+create database Students;
+show databases;
+use Students;
+create table academics(Name varchar(25),RollNo varchar(8),Dept Char(3),Mark1 integer(3),Mark2 integer(3),Mark3 integer(3),Mark4 integer(3),Mark5 integer(3),Mark6 integer(3),Mark7 integer(3),Rank integer(3));
